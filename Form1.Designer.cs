c7880841a0f21f8319dce63caa9a002ee0a3c8ef
Karn <churikawit@gmail.com>
2022-05-23 14:38:50 +0700

    Initial project

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace CalculateInstallment
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_pv = new System.Windows.Forms.TextBox();
+            this.textBox_intrate = new System.Windows.Forms.TextBox();
+            this.textBox_term_year = new System.Windows.Forms.TextBox();
+            this.textBox_installment = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_term_month = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_pv
+            // 
+            this.textBox_pv.Location = new System.Drawing.Point(130, 37);
+            this.textBox_pv.Name = "textBox_pv";
+            this.textBox_pv.Size = new System.Drawing.Size(100, 23);
+            this.textBox_pv.TabIndex = 0;
+            this.textBox_pv.Text = "0";
+            // 
+            // textBox_intrate
+            // 
+            this.textBox_intrate.Location = new System.Drawing.Point(130, 78);
+            this.textBox_intrate.Name = "textBox_intrate";
+            this.textBox_intrate.Size = new System.Drawing.Size(100, 23);
+            this.textBox_intrate.TabIndex = 1;
+            this.textBox_intrate.Text = "0";
+            // 
+            // textBox_term_year
+            // 
+            this.textBox_term_year.Location = new System.Drawing.Point(130, 116);
+            this.textBox_term_year.Name = "textBox_term_year";
+            this.textBox_term_year.Size = new System.Drawing.Size(100, 23);
+            this.textBox_term_year.TabIndex = 2;
+            this.textBox_term_year.Text = "0";
+            // 
+            // textBox_installment
+            // 
+            this.textBox_installment.Location = new System.Drawing.Point(130, 157);
+            this.textBox_installment.Name = "textBox_installment";
+            this.textBox_installment.Size = new System.Drawing.Size(100, 23);
+            this.textBox_installment.TabIndex = 3;
+            this.textBox_installment.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "เงินต้น";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "อัตราดอกเบี้ย";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ระยะเวลาผ่อนชำระ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "เงินงวดต่อเดือน";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(245, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ปี";
+            // 
+            // textBox_term_month
+            // 
+            this.textBox_term_month.Location = new System.Drawing.Point(265, 116);
+            this.textBox_term_month.Name = "textBox_term_month";
+            this.textBox_term_month.Size = new System.Drawing.Size(100, 23);
+            this.textBox_term_month.TabIndex = 9;
+            this.textBox_term_month.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(380, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "เดือน";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(236, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "ต่อปี";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 287);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_term_month);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_installment);
+            this.Controls.Add(this.textBox_term_year);
+            this.Controls.Add(this.textBox_intrate);
+            this.Controls.Add(this.textBox_pv);
+            this.Name = "Form1";
+            this.Text = "Find Installment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_pv;
+        private System.Windows.Forms.TextBox textBox_intrate;
+        private System.Windows.Forms.TextBox textBox_term_year;
+        private System.Windows.Forms.TextBox textBox_installment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_term_month;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateInstallment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Decimal principle, intrate, term_year, term_month, installment, term;
+            try
+            {
+                if (textBox_pv.Text == "")
+                    textBox_pv.Text = "0";
+                principle = Decimal.Parse(textBox_pv.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("เงินต้น ผิดพลาด");
+                return;
+            }
+            try
+            {
+                if (textBox_intrate.Text == "")
+                    textBox_intrate.Text = "0";
+                intrate = Decimal.Parse(textBox_intrate.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("อัตราดอกเบี้ย ผิดพลาด");
+                return;
+            }
+            try
+            {
+                if (textBox_term_year.Text == "")
+                    textBox_term_year.Text = "0";
+                term_year = Decimal.Parse(textBox_term_year.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ระยะเวลาผ่อนชำระ ผิดพลาด");
+                return;
+            }
+            try
+            {
+                if (textBox_term_month.Text == "")
+                    textBox_term_month.Text = "0";
+                term_month = Decimal.Parse(textBox_term_month.Text);
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("ระยะเวลาผ่อนชำระ ผิดพลาด");
+                return;
+            }
+            try
+            {
+                if (textBox_installment.Text == "")
+                    textBox_installment.Text = "0";
+                installment = Decimal.Parse(textBox_installment.Text);
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("เงินงวดผ่อนชำระ ผิดพลาด");
+                return;
+            }
+
+            term = term_year * 12 + term_month;
+
+            if (principle > 0 && intrate > 0 && term > 0 && installment == 0)
+            {
+                installment = find_installment(principle, intrate, term);
+                textBox_installment.Text = String.Format("{0}", installment);
+            }
+            else if (principle > 0 && intrate > 0 && term == 0 && installment > 0)
+            {
+                term = find_term(principle, intrate, installment);
+                if (term > 0)
+                {
+                    textBox_term_year.Text = String.Format("{0}", Decimal.ToInt32(term) / 12);
+                    int m = Decimal.ToInt32(term) % 12;
+                    textBox_term_month.Text = String.Format("{0}", m);
+                }
+                else 
+                {
+                    MessageBox.Show("การคำนวณไม่สำเร็จ");
+                    return;
+                }
+            }
+            else if (principle > 0 && intrate == 0 && term > 0 && installment > 0)
+            {
+                intrate = find_intrate(principle, term, installment);
+                if (intrate > 0) 
+                {
+                    textBox_intrate.Text = String.Format("{0}", intrate);
+                }
+                else
+                {
+                    MessageBox.Show("การคำนวณไม่สำเร็จ");
+                    return;
+                }
+            }
+            else if (principle == 0 && intrate > 0 && term > 0 && installment > 0)
+            {
+                principle = find_principle(intrate, term, installment);
+                textBox_pv.Text = String.Format("{0}", principle);
+            }
+            else
+            {
+                MessageBox.Show("เงื่อนไขการคำนวณไม่ถูกต้อง");
+                return;
+            }
+        }
+
+        public static Decimal Decimal_Pow(Decimal x, Decimal y)
+        {
+            return new Decimal(System.Math.Pow(Decimal.ToDouble(x), Decimal.ToDouble(y)));
+        }
+        public static Decimal Decimal_RoundUp(Decimal d, int decimals)
+        {
+            Decimal output = d;
+            bool minus_flag = false;
+            if (output < 0)
+            {
+                output = -output;
+                minus_flag = true;
+            }
+
+            if (decimals <= 0)
+            {
+                decimals = decimals * -1;
+                Decimal devider = 1;
+                for (int i = 0; i < decimals; i++)
+                {
+                    devider = devider * 10;
+                }
+                // ปัดค่าเงินงวด
+                Decimal temp = output / devider;
+                if (output > Decimal.ToInt32(temp) * devider)
+                {
+                    output = (Decimal.ToInt32(temp) + 1) * devider;
+                }
+            }
+            else
+            {
+                Decimal multiplier = 1;
+                for (int i = 0; i < decimals; i++)
+                {
+                    multiplier = multiplier * 10;
+                }
+                // ปัดค่าเงินงวด
+                Decimal temp = output * multiplier;
+                if (output > Decimal.ToInt32(temp) / multiplier)
+                {
+                    output = (Decimal.ToInt32(temp) + 1) / multiplier;
+                }
+            }
+
+            if (minus_flag == true) output = -output;
+            return output;
+        }
+        public static Decimal Decimal_RoundDown(Decimal d, int decimals)
+        {
+            Decimal output = d;
+            bool minus_flag = false;
+            if (output < 0)
+            {
+                output = -output;
+                minus_flag = true;
+            }
+
+            if (decimals <= 0)
+            {
+                decimals = decimals * -1;
+                Decimal devider = 1;
+                for (int i = 0; i < decimals; i++)
+                {
+                    devider = devider * 10;
+                }
+                // ปัดค่าเงินงวด
+                Decimal temp = output / devider;
+                output = Decimal.ToInt32(temp) * devider;
+            }
+            else
+            {
+                Decimal multiplier = 1;
+                for (int i = 0; i < decimals; i++)
+                {
+                    multiplier = multiplier * 10;
+                }
+                // ปัดค่าเงินงวด
+                Decimal temp = output * multiplier;
+                output = Decimal.ToInt32(temp) / multiplier;
+            }
+
+            if (minus_flag == true) output = -output;
+            return output;
+        }
+
+        private Decimal find_installment(Decimal principle, Decimal intrate, Decimal term) {
+            Decimal emi = 0;
+            Decimal dummy1;
+            Decimal dummy2;
+            try
+            {
+                dummy1 = intrate / new Decimal(12.0) / new Decimal(100.0);
+            }
+            catch (System.OverflowException e)
+            {
+                throw e;
+            }
+            try
+            {
+                dummy2 = Decimal_Pow(dummy1 + 1, term);
+            }
+            catch (System.OverflowException e)
+            {
+                throw e;
+            }
+            try
+            {
+                emi = principle * (dummy1 / (1 - 1 / dummy2));
+            }
+            catch (System.OverflowException e)
+            {
+                throw e;
+            }
+
+            // ปัดค่าเงินงวด ให้เต็มร้อย
+            emi = Decimal_RoundUp(emi, -2);
+
+            return emi;
+        }
+
+        private Decimal find_term(Decimal principle, Decimal intrate, Decimal installment)
+        {
+            Decimal term = 0;
+            Decimal test_principle, test_term = 0;
+            int i = 0;
+            const int MAX_TERM_YEAR = 41;
+
+            for (i = 1; i <= MAX_TERM_YEAR; i++) 
+            {
+                test_term = i * 12;
+                test_principle = find_principle(intrate, test_term, installment);
+
+                if (test_principle >= principle) {
+                    break;
+                }
+            }
+            if (i > MAX_TERM_YEAR)
+            {
+                // out of range
+                return 0;
+            }
+
+            int test_term_start = Decimal.ToInt32(test_term) - 12;
+            if (test_term_start <= 0)
+                test_term_start = 1;
+            for (i = test_term_start; i <= test_term; i++) {
+                test_principle = find_principle(intrate, i, installment);
+                if (test_principle >= principle) {
+                    term = i;
+                    break;
+                }
+            }
+
+            return term;
+        }
+
+        private Decimal find_intrate(Decimal principle, Decimal term, Decimal installment)
+        {
+            Decimal intrate = 0, test_intrate = 0, test_principle = 0;
+            const int MAX_INTRATE = 15;
+            int i = 0;
+            for (i = 1; i <= MAX_INTRATE; i++)
+            {
+                test_intrate = i;
+                test_principle = find_principle(test_intrate, term, installment);
+
+                if (test_principle < principle) 
+                {
+                    break;
+                }
+            }
+            if (i > MAX_INTRATE)
+            {
+                // out of range
+                return 0;
+            }
+
+            test_intrate = test_intrate - 1;
+            for (i = 1; i <= 100; i++) {
+                test_intrate = test_intrate + new decimal(0.01);
+                test_principle = find_principle(test_intrate, term, installment);
+                if (test_principle < principle)
+                {
+                    break;
+                }
+            }
+
+            test_intrate = test_intrate - new decimal(0.01);
+            for (i = 1; i <= 100; i++)
+            {
+                test_intrate = test_intrate + new decimal(0.0001);
+                test_principle = find_principle(test_intrate, term, installment);
+                if (test_principle < principle)
+                {
+                    break;
+                }
+            }
+            intrate = test_intrate - new decimal(0.0001);
+
+            return intrate;
+        }
+
+        private Decimal find_principle(Decimal intrate, Decimal term, Decimal installment)
+        {
+            Decimal principle = 0;
+            Decimal dummy1, dummy2;
+
+            try
+            {
+                dummy1 = intrate / new Decimal(1200.0);
+            }
+            catch (System.OverflowException e)
+            {
+                throw e;
+            }
+            try
+            {
+                dummy2 = Decimal_Pow(dummy1 + 1, term);
+            }
+            catch (System.OverflowException e)
+            {
+                throw e;
+            }
+            try
+            {
+                principle = installment * ( (1 - 1 / dummy2) / dummy1);
+            }
+            catch (System.OverflowException e)
+            {
+                throw e;
+            }
+
+            // ปัดลงหลักพัน
+            principle = Decimal_RoundDown(principle, -3);
+
+            return principle;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
